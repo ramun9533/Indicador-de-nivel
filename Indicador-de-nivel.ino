a7286e4c18da56c338e90cdf23b44741d7e4bb50
William Eli Orta Lopez <ramun346416@gmail.com>
2021-05-17 18:03:14 -0500

    Buaneas practicas de programacion
    [Se modifica nombres de variables para que vaya mas acorde a las buenas practicas de programacion

--- Indicador-de-nivel.ino
+++ Indicador-de-nivel.ino
@@ -1,30 +1,30 @@
-uint8_t l1 = 2;
-uint8_t l2 = 3; // abreviacion (uint8_t) enteros de 1 byte sin signo
-uint8_t l3 = 4; //
-uint8_t l4 = 5;
-uint8_t l5 = 6 ; //
-uint8_t o1 = 7; //
-uint8_t o2 = 8; //
-uint8_t o3 = 9; //
-uint8_t o4 = 10; //
-uint8_t o5 = 11 ; //
-uint8_t o6 = 12 ; 
-int L1, L2, L3, L4, L5; //variables donde guardare las entradas para hacer la logica
+int8_t in1 = 2;
+uint8_t in2 = 3; // abreviacion (uint8_t) enteros de 1 byte sin signo
+uint8_t in3 = 4; //
+uint8_t in4 = 5;
+uint8_t in5 = 6 ; //
+uint8_t out1 = 7; //
+uint8_t out2 = 8; //
+uint8_t out3 = 9; //
+uint8_t out4 = 10; //
+uint8_t out5 = 11 ; //
+uint8_t out6 = 12 ;
+int Nivel1 , Nivel2, Nivel3, Nivel4, Nivel5; //variables donde guardare las entradas para hacer la logica
 int O1, O2, O3, O4, O5, O6, nivel, nivel1; // variables donde reflegare las entradas en forma de salidas, y "nivel" donde guardare el nivel
 char nivel3;
 void setup() {
   Serial.begin (9600);
-  pinMode(l1, INPUT);
-  pinMode(l2, INPUT);
-  pinMode(l3, INPUT);
-  pinMode(l4, INPUT);
-  pinMode(l5, INPUT);
-  pinMode(o1, OUTPUT);
-  pinMode(o2, OUTPUT);
-  pinMode(o3, OUTPUT);
-  pinMode(o4, OUTPUT);
-  pinMode(o5, OUTPUT);
-  pinMode(o6, OUTPUT);
+  pinMode(in1, INPUT);
+  pinMode(in2, INPUT);
+  pinMode(in3, INPUT);
+  pinMode(in4, INPUT);
+  pinMode(in5, INPUT);
+  pinMode(out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
+  pinMode(out4, OUTPUT);
+  pinMode(out5, OUTPUT);
+  pinMode(out6, OUTPUT);
   nivel3 = 'A';
   Serial.println (nivel3);
 }
@@ -35,57 +35,57 @@ void imprime ()
 }
 
 void loop() {
-  L1 = digitalRead(l1);
-  L2 = digitalRead(l2);
-  L3 = digitalRead(l3);
-  L4 = digitalRead(l4);
-  L5 = digitalRead(l5);
-  digitalWrite(o2, L2);
-  digitalWrite(o3, L3);
-  digitalWrite(o4, L4);
-  digitalWrite(o5, L5);
-  if (!L1 and !L2 and !L3 and !L4 and !L5) //inicia el algebra boleana
+  Nivel1 = digitalRead(in1);
+  Nivel2 = digitalRead(in2);
+  Nivel3 = digitalRead(in3);
+  Nivel4 = digitalRead(in4);
+  Nivel5 = digitalRead(in5);
+  digitalWrite(out2, Nivel2);
+  digitalWrite(out3, Nivel3);
+  digitalWrite(out4, Nivel4);
+  digitalWrite(out5, Nivel5);
+  if (!Nivel1 and !Nivel2 and !Nivel3 and !Nivel4 and !Nivel5) //inicia el algebra boleana
   {
     nivel = 0;
     nivel3 = 'A';
   }
 
-  if (L1 == 1)
+  if (Nivel1 == 1)
   {
     nivel = 20;
     nivel3 = 'B';
   }
-  if (L2 == 1)
+  if (Nivel2 == 1)
   {
     nivel = 40;
     nivel3 = 'C';
   }
-  if (L3 == 1)
+  if (Nivel3 == 1)
   { nivel = 60;
     nivel3 = 'D';
   }
 
-  if (L4 == 1)
+  if (Nivel4 == 1)
   { nivel = 80;
     nivel3 = 'E';
   }
-  if (L5 == 1)
+  if (Nivel5 == 1)
   { nivel = 5;
     nivel3 = 'F';
   }
-  if (!((L1 and L2 and L3 and L4 and L5) or
-        (L1 and L2 and L3 and L4 and !L5) or
-        (L1 and L2 and L3 and !L4 and !L5) or
-        (L1 and L2 and !L3 and !L4 and !L5) or
-        (L1 and !L2 and !L3 and !L4 and !L5) or
-        (!L1 and !L2 and !L3 and !L4 and !L5)  ) )
+  if (!((Nivel1 and Nivel2 and Nivel3 and Nivel4 and Nivel5) or
+        (Nivel1 and Nivel2 and Nivel3 and Nivel4 and !Nivel5) or
+        (Nivel1 and Nivel2 and Nivel3 and !Nivel4 and !Nivel5) or
+        (Nivel1 and Nivel2 and !Nivel3 and !Nivel4 and !Nivel5) or
+        (Nivel1 and !Nivel2 and !Nivel3 and !Nivel4 and !Nivel5) or
+        (!Nivel1 and !Nivel2 and !Nivel3 and !Nivel4 and !Nivel5)  ) )
   {
     nivel = 6;
     Serial.println ('G');
     //   Serial.println ("Error, revise el sistema comenzando por el sensor indicador"); // modificar secuencuia de error
-    digitalWrite(o6, HIGH);
+    digitalWrite(out6, HIGH);
     delay(250);
-    digitalWrite(o6, LOW);
+    digitalWrite(out6, LOW);
     delay(250);
   }
   if (nivel1 != nivel)                                   // hace algo diferente
